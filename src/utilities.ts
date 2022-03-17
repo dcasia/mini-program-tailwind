298edcedfc054fe0a62230512e2ba0ac80a5e17d
Ryan Wu <imgreenwu@gmail.com>
2022-03-17 20:06:24 +0800

    compatible with comma symbol

--- src/utilities.ts
+++ src/utilities.ts
@@ -8,29 +8,43 @@ export function isTemplateFile(filename) {
     return /.+\.(wx|ax|jx|ks|tt|q)ml$/.test(filename)
 }
 
-export const specialCharactersMap = {
-    [ '[' ]: '-',
-    [ ']' ]: '-',
-    [ '(' ]: '-',
-    [ ')' ]: '-',
-    [ '#' ]: '-h-',
-    [ '!' ]: '-i-',
-    [ '/' ]: '-s-',
-    [ '.' ]: '-d-',
-    [ ':' ]: '-c-',
+const charactersMap = {
+    '[': '-',
+    ']': '-',
+    '(': '-',
+    ')': '-',
+    '#': '-h-',
+    '!': '-i-',
+    '/': '-s-',
+    '.': '-d-',
+    ':': '-c-',
+    ',': '-2c-',
 }
 
-export const backslasheMap = {
+const specialCharactersMap = {
+    '\\\\2c\\s': '-2c-',
+}
+
+const backslashMap = {
     [ FileType.Style ]: '\\\\\\',
     [ FileType.Template ]: '\\',
 }
 
 export function handleCharacters(content, type: FileType) {
 
+    for (const from in charactersMap) {
+
+        const to = charactersMap[ from ]
+        const regExp = new RegExp(`${ backslashMap[ type ] }${ from }`, 'g')
+
+        content = content.replace(regExp, to)
+
+    }
+
     for (const from in specialCharactersMap) {
 
         const to = specialCharactersMap[ from ]
-        const regExp = new RegExp(`${ backslasheMap[ type ] }${ from }`, 'g')
+        const regExp = new RegExp(`${ from }`, 'g')
 
         content = content.replace(regExp, to)
 
