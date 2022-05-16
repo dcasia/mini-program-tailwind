c828f41037be777b7ce32ce2474415221d568d6f
Ryan Wu <imgreenwu@gmail.com>
2022-05-16 12:35:20 +0800

    add Vue3 example

---
+++ examples/taro/vue-3/src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="index bg-[#b5b5b5] h-screen pl-7.5" :class="{'text-[50px]': true}">
+    <text :class="['font-bold', 'text-[#ffffff]']">{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.css'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
