e48d27fa5d465f2241e68b497e809e64119683e4
Ryan Wu <imgreenwu@gmail.com>
2022-05-17 19:12:17 +0800

    handle cases for Taro h5 compilation

--- src/enum.ts
+++ src/enum.ts
@@ -3,8 +3,19 @@ export enum FileType {
     Style = 'style',
 }
 
-export enum FrameworkUsedInTaro {
+export enum TaroFramework {
     React = 'react',
     Vue2 = 'vue',
     Vue3 = 'vue3',
+}
+
+export enum TaroPlatform {
+    Wechat = 'weapp',
+    Swan = 'swan',
+    Alipay = 'alipay',
+    TT = 'tt',
+    QQ = 'qq',
+    JD = 'jd',
+    Quick = 'quickapp',
+    H5 = 'h5',
 }
