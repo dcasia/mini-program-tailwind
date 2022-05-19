b8a796e1fc3af4725147139f20e5eb9ed9808170
Ryan Wu <imgreenwu@gmail.com>
2022-05-20 02:15:18 +0800

    refactor class name match method in the ideal way

--- src/babel/index.ts
+++ src/babel/index.ts
@@ -2,21 +2,31 @@ import { FileType, TaroFramework } from '../enum'
 import { handleCharacters } from '../utilities'
 import { PluginItem } from '@babel/core'
 import * as babel from '@babel/core'
-import { handleClassNameWithCurlyBraces } from '../template-handler'
+import { handleClassNameInTemplate } from '../template-handler'
 
 let classFieldsChanges: [string, string][] = []
 
 const classFieldName = {
     [ TaroFramework.React ]: [ 'className' ],
     [ TaroFramework.Vue2 ]: [ 'class', 'staticClass' ],
-    [ TaroFramework.Vue3 ]: [ 'class', 'staticClass' ],
+    [ TaroFramework.Vue3 ]: [ 'class' ],
 }
 
-function logClassFieldsChange(rawContent: string, newContent: string) {
+function isIdentifierTheClassField(path: babel.NodePath<babel.types.ObjectProperty>, framework: TaroFramework) {
+
+    const targetClassFieldName = classFieldName[ framework ] || []
+    const key = path.get('key')
+    const keyName = key.isIdentifier() && key.node.name
+
+    return targetClassFieldName.includes(keyName)
+
+}
+
+function recordClassNameChanges(rawContent: string, newContent: string) {
     classFieldsChanges.push([ rawContent, newContent ])
 }
 
-function cleanClassFieldsChanges() {
+function cleanClassNameChanges() {
     classFieldsChanges = []
 }
 
@@ -44,36 +54,48 @@ export function replaceStringLiteralPlugin({ types: t }): PluginItem {
 
 export function replaceClassFieldsValuePlugin({ types: t }): PluginItem {
 
+    let isVisitingClassNameField = false
+
     return {
         visitor: {
-            StringLiteral(path, state) {
 
-                const targetClassFieldName = classFieldName[ state.opts.framework ] || []
+            /**
+             * Assuming there shouldn't have nested class fields in any cases
+             */
+            ObjectProperty: {
+                enter(path, state) {
 
-                const keyNode = path.key === 'value' && path.parentPath.isObjectProperty() && path.parentPath.get('key') // universal class: 'test'
-                    || (path.parentPath.isArrayExpression() && path.parentPath.parentPath.isObjectProperty() && path.parentPath.parentPath.get('key')) // vue class: ['test', 'test']
-                    || (path.parentPath.isMemberExpression() && path.parentPath.parentPath.isCallExpression() && path.parentPath.parentPath.parentPath.isObjectProperty() && path.parentPath.parentPath.parentPath.get('key')) // react className: 'test'.concat('test-1', 'test-2')
-                    || (path.parentPath.isCallExpression() && path.parentPath.parentPath.isObjectProperty() && path.parentPath.parentPath.get('key')) // react className: 'test'.concat('test-1', 'test-2')
-                    || (path.parentPath.isConditionalExpression() && path.parentPath.parentPath.isCallExpression() && path.parentPath.parentPath.parentPath.isObjectProperty() && path.parentPath.parentPath.parentPath.get('key')) // react className: 'test'.concat(true ? 'test-1' : '')
-                    || (path.key === 'key' && path.parentPath.findParent(path => path.isObjectProperty())?.get('key')) // vue class: ['test', {'test': true}] or class: {'test': true}
-                    || (path.parentPath.isArrayExpression() && path.parentPath.findParent(path => path.isObjectProperty())?.get('key')) // vue class: Object(['test'])
+                    if (isIdentifierTheClassField(path, state.opts.framework)) {
 
-                if (keyNode && !Array.isArray(keyNode)) {
+                        console.log('enter')
+                        isVisitingClassNameField = true
 
-                    const foundClassName = targetClassFieldName.find(name => keyNode.isIdentifier?.({ name }))
+                    }
 
-                    if (foundClassName) {
+                },
+                exit(path, state) {
 
-                        const rawContent = path.node.value
-                        const newContent = handleClassNameWithCurlyBraces(rawContent)
+                    if (isIdentifierTheClassField(path, state.opts.framework)) {
 
-                        if (newContent !== rawContent) {
+                        console.log('exit')
+                        isVisitingClassNameField = false
 
-                            logClassFieldsChange(rawContent, newContent)
-                            path.replaceWith(t.stringLiteral(newContent))
-                            path.skip()
+                    }
 
-                        }
+                },
+            },
+            StringLiteral(path) {
+
+                if (isVisitingClassNameField) {
+
+                    const rawContent = path.node.value
+                    const newContent = handleClassNameInTemplate(rawContent)
+
+                    if (newContent !== rawContent) {
+
+                        recordClassNameChanges(rawContent, newContent)
+                        path.replaceWith(t.stringLiteral(newContent))
+                        path.skip()
 
                     }
 
@@ -87,7 +109,7 @@ export function replaceClassFieldsValuePlugin({ types: t }): PluginItem {
 
 export function collectRawAndModified(rawContent: string, framework: TaroFramework) {
 
-    cleanClassFieldsChanges()
+    cleanClassNameChanges()
 
     babel.transformSync(rawContent, {
         plugins: [
